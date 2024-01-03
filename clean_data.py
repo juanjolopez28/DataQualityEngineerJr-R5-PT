5438c7394dbb7141b8b6c6bf54ff3b4290d564c0
juanjolopez28 <jlopezco@unsa.edu.pe>
2024-01-02 21:55:22 -0500

    Preparando repositorio

---
+++ clean_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+import json
+
+def json_to_csv():
+    #Verificamos encontrar la ruta respectiva en cualquier entorno
+    ruta_script = os.path.abspath(__file__)
+    directorio_actual = os.path.dirname(ruta_script)
+    ruta_json = os.path.join(directorio_actual, 'DB', 'taylor_swift_spotify.json')
+
+    with open(ruta_json, 'r') as json_file:
+        data = pd.read_json(json_file)
+    data.to_csv('archivo.csv', index=False)
+
+if __name__ == "__main__":
+    json_to_csv()
